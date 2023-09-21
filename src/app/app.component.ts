7f885f53eaa846e3d7588a4054e6db5ff7be83cf
matteo9966 <matteo_tommasi96@hotmail.com>
2023-09-21 19:02:36 +0200

    initial commit and created a theming system for the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import {MatButtonModule} from '@angular/material/button'
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatButtonModule,MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
